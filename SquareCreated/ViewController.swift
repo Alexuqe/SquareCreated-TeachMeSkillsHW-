c753e585d33c38c764b337b2a553093a0a0502c6
alex <hordiah1@google.com>
2024-12-10 17:28:37 +0300

    create SquareCreatedApp *TeachMeSkillsHW)

--- SquareCreated/ViewController.swift
+++ SquareCreated/ViewController.swift
@@ -1,19 +1,114 @@
-//
-//  ViewController.swift
-//  SquareCreated
-//
-//  Created by Sasha on 10.12.24.
-//
+    //
+    //  ViewController.swift
+    //  SquareCreated
+    //
+    //  Created by Sasha on 10.12.24.
+    //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+
+    let button = UIButton()
+
+    let safeScreen: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    let spacing: CGFloat = 5
+    var currentX: CGFloat = 0
+    var currentY: CGFloat = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+
+        elementConfig()
+    }
+
+    func elementConfig() {
+        configureButton()
+        configureSafeScreen()
+    }
+
+    func configureSafeScreen() {
+        view.addSubview(safeScreen)
+        constraintsSafeScreen()
+    }
+
+    func configureButton() {
+        var configure = UIButton.Configuration.filled()
+        configure.title = "Tap Me"
+        configure.baseBackgroundColor = .systemGray4
+        configure.baseForegroundColor = .white
+        configure.cornerStyle = .capsule
+        configure.titleAlignment = .center
+
+        button.configuration = configure
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(button)
+        constraintsButton()
+
+        button.addTarget(self, action: #selector(createSquare), for: .touchUpInside)
+    }
+
+    @objc func createSquare() {
+        let newView = UIView()
+        newView.backgroundColor = .gray
+        newView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(newView)
+
+        let safeAreaWidth = safeScreen.frame.width
+        let safeAreaHeight = safeScreen.frame.height
+        let squareSize = safeScreen.frame.width / 5
+
+        if currentX + (squareSize + spacing) >= safeAreaWidth {
+            currentX = 0
+            currentY += squareSize + spacing
+        }
+
+        if currentY + (squareSize + spacing) > safeAreaHeight {
+            return
+        }
+
+        NSLayoutConstraint.activate([
+            newView.leadingAnchor.constraint(equalTo: safeScreen.leadingAnchor, constant: currentX),
+            newView.topAnchor.constraint(equalTo: safeScreen.topAnchor, constant: currentY),
+            newView.heightAnchor.constraint(equalToConstant: squareSize),
+            newView.widthAnchor.constraint(equalToConstant: squareSize)
+        ])
+
+        currentX += squareSize + spacing
+    }
+
+    func constraintsSafeScreen() {
+        NSLayoutConstraint.activate([
+            safeScreen.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            safeScreen.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 20), // Добавьте эту строку
+            safeScreen.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20), // Добавьте эту строку
+            safeScreen.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -40) // Оставляем высоту фиксированной
+        ])
     }
 
 
+    func constraintsButton() {
+        NSLayoutConstraint.activate([
+            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            button.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            button.widthAnchor.constraint(equalToConstant: 180),
+            button.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+
+
+}
+
+#Preview {
+    let view = ViewController()
+    view
 }
 
